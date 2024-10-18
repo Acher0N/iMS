07fde6211cf6782851c7d21df2ea1650ce5e8b7d
Knight Fury <kingsmandev.co@gmail.com>
2024-10-18 22:20:52 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,109 @@
+import { Box, Button, Typography } from "@mui/material";
+
 const App: React.FC = () => {
-  return <div>App</div>;
+  const commonStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  };
+  return (
+    <Box
+      sx={{ display: "flex", flexDirection: "column", gap: "50px", p: "30px" }}
+    >
+      <Box sx={{ ...commonStyle }}>
+        <Box sx={{ minWidth: "100px" }}>Text</Box>
+        <Button variant="text" color="success">
+          success
+        </Button>
+        <Button variant="text" color="info">
+          info
+        </Button>
+        <Button variant="text" color="error">
+          error
+        </Button>
+        <Button variant="text" color="warning">
+          warn
+        </Button>
+      </Box>
+      <Box sx={{ ...commonStyle }}>
+        <Box sx={{ minWidth: "100px" }}>Text</Box>
+        <Button variant="text" color="success" size="small">
+          success
+        </Button>
+        <Button variant="text" color="info" size="small">
+          info
+        </Button>
+        <Button variant="text" color="error" size="small">
+          error
+        </Button>
+        <Button variant="text" color="warning" size="small">
+          warn
+        </Button>
+      </Box>
+
+      <Box sx={{ ...commonStyle }}>
+        <Box sx={{ minWidth: "100px" }}>Outlined</Box>
+        <Button variant="outlined" color="success">
+          success
+        </Button>
+        <Button variant="outlined" color="info">
+          info
+        </Button>
+        <Button variant="outlined" color="error">
+          error
+        </Button>
+        <Button variant="outlined" color="warning">
+          warn
+        </Button>
+      </Box>
+      <Box sx={{ ...commonStyle }}>
+        <Box sx={{ minWidth: "100px" }}>Outlined</Box>
+        <Button variant="outlined" color="success" size="small">
+          success
+        </Button>
+        <Button variant="outlined" color="info" size="small">
+          info
+        </Button>
+        <Button variant="outlined" color="error" size="small">
+          error
+        </Button>
+        <Button variant="outlined" color="warning" size="small">
+          warn
+        </Button>
+      </Box>
+
+      <Box sx={{ ...commonStyle }}>
+        <Box sx={{ minWidth: "100px" }}>Contained</Box>
+        <Button variant="contained" color="success">
+          success
+        </Button>
+        <Button variant="contained" color="info">
+          info
+        </Button>
+        <Button variant="contained" color="error">
+          error
+        </Button>
+        <Button variant="contained" color="warning">
+          warn
+        </Button>
+      </Box>
+      <Box sx={{ ...commonStyle }}>
+        <Box sx={{ minWidth: "100px" }}>Contained</Box>
+        <Button variant="contained" color="success" size="small">
+          success
+        </Button>
+        <Button variant="contained" color="info" size="small">
+          info
+        </Button>
+        <Button variant="contained" color="error" size="small">
+          error
+        </Button>
+        <Button variant="contained" color="warning" size="small">
+          warn
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default App;
